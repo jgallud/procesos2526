c9b7944413b55d174185272d36a246c310936631
José A. Gallud <jgallud@gmail.com>
2025-10-28 17:17:05 +0100

    ojo version local

--- cliente/clienteRest.js
+++ cliente/clienteRest.js
@@ -1,4 +1,55 @@
 function ClienteRest() {
+    this.registrarUsuario = function (email, password) {
+        $.ajax({
+            type: 'POST',
+            url: '/registrarUsuario',
+            data: JSON.stringify({ "email": email, "password": password }),
+            success: function (data) {
+                if (data.nick != -1) {
+                    console.log("Usuario " + data.nick + " ha sido registrado");
+                    $.cookie("nick", data.nick);
+                    //cw.limpiar();
+                    //cw.mostrarMsg("Bienvenido al sistema,"+data.nick);
+                    cw.mostrarLogin();
+                }
+                else {
+                    console.log("El nick está ocupado");
+                    cw.mostrarModal("El nick está ocupado");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    }
+     this.loginUsuario = function (email, password) {
+        $.ajax({
+            type: 'POST',
+            url: '/loginUsuario',
+            data: JSON.stringify({ "email": email, "password": password }),
+            success: function (data) {
+                if (data.nick != -1) {
+                    console.log("Usuario " + data.nick + " ha iniciado sesión");
+                    $.cookie("nick", data.nick);
+                    //ws.email=data.nick;
+                    cw.limpiar();
+                    cw.mostrarHome();
+                    //cw.mostrarMsg("Bienvenido al sistema,"+data.nick);
+                    //cw.mostrarLogin();
+                }
+                else {
+                    console.log("Usuario o clave incorrectos");
+                }
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log("Status: " + textStatus);
+                console.log("Error: " + errorThrown);
+            },
+            contentType: 'application/json'
+        });
+    }
     this.agregarUsuario = function (nick) {
         var cli = this;
         $.getJSON("/agregarUsuario/" + nick, function (data) {
