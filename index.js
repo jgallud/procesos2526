e07f2f9f68d9a49a4952e4a9ef21397a2d515ec3
jgallud <jgallud@gmail.com>
2025-11-03 10:57:45 +0100

    google sign in

--- index.js
+++ index.js
@@ -28,108 +28,117 @@ passport.use(new
         }
     ));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const haIniciado = function (request, response, next) {
+    if (request.user) {
+        next();
+    }
+    else {
+        response.redirect("/")
+    }
+}
 
-let sistema = new modelo.Sistema();
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-    var contenido = fs.readFileSync(__dirname + "/cliente/index.html");
-    response.setHeader("Content-type", "text/html");
-    response.send(contenido);
+    let sistema = new modelo.Sistema();
 
-});
+    app.get("/", function (request, response) {
+        var contenido = fs.readFileSync(__dirname + "/cliente/index.html");
+        response.setHeader("Content-type", "text/html");
+        response.send(contenido);
 
-app.get("/auth/google", passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get('/google/callback',
-    passport.authenticate('google', { failureRedirect: '/fallo' }),
-    function (req, res) {
-        res.redirect('/good');
     });
 
-app.post('/oneTap/callback',
-    passport.authenticate('google-one-tap', { failureRedirect: '/fallo' }),
-    function (req, res) {
-        // Successful authentication, redirect home.
-        res.redirect('/good');
+    app.get("/auth/google", passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+    app.get('/google/callback',
+        passport.authenticate('google', { failureRedirect: '/fallo' }),
+        function (req, res) {
+            res.redirect('/good');
+        });
+
+    app.post('/oneTap/callback',
+        passport.authenticate('google-one-tap', { failureRedirect: '/fallo' }),
+        function (req, res) {
+            // Successful authentication, redirect home.
+            res.redirect('/good');
+        });
+
+    // app.get("/good", function (request, response) {
+    //     let nick = request.user.emails[0].value;
+    //     if (nick) {
+    //         sistema.agregarUsuario(nick);
+    //     }
+    //     //console.log(request.user.emails[0].value);
+    //     response.cookie('nick', nick);
+    //     response.redirect('/');
+    // });
+
+    app.get("/good", function (request, response) {
+        let email = request.user.emails[0].value;
+        sistema.usuarioGoogle({ "email": email }, function (obj) {
+            response.cookie('nick', obj.email);
+            response.redirect('/');
+        });
     });
 
-// app.get("/good", function (request, response) {
-//     let nick = request.user.emails[0].value;
-//     if (nick) {
-//         sistema.agregarUsuario(nick);
-//     }
-//     //console.log(request.user.emails[0].value);
-//     response.cookie('nick', nick);
-//     response.redirect('/');
-// });
-
-app.get("/good", function (request, response) {
-    let email = request.user.emails[0].value;
-    sistema.usuarioGoogle({ "email": email }, function (obj) {
-        response.cookie('nick', obj.email);
-        response.redirect('/');
+    app.get("/fallo", function (request, response) {
+        response.send({ nick: "nook" })
     });
-});
 
-app.get("/fallo", function (request, response) {
-    response.send({ nick: "nook" })
-});
-
-app.post("/registrarUsuario", function (request, response) {
-    sistema.registrarUsuario(request.body, function (res) {
-        response.send({ "nick": res.email });
+    app.post("/registrarUsuario", function (request, response) {
+        sistema.registrarUsuario(request.body, function (res) {
+            response.send({ "nick": res.email });
+        });
     });
-});
 
-app.get("/confirmarUsuario/:email/:key", function (request, response) {
-    let email = request.params.email;
-    let key = request.params.key;
-    sistema.confirmarUsuario({ "email": email, "key": key }, function (usr) {
-        if (usr.email != -1) {
-            response.cookie('nick', usr.email);
-        }
-        response.redirect('/');
+    app.get("/confirmarUsuario/:email/:key", function (request, response) {
+        let email = request.params.email;
+        let key = request.params.key;
+        sistema.confirmarUsuario({ "email": email, "key": key }, function (usr) {
+            if (usr.email != -1) {
+                response.cookie('nick', usr.email);
+            }
+            response.redirect('/');
+        });
+    })
+
+    app.post('/loginUsuario', passport.authenticate("local", { failureRedirect: "/fallo", successRedirect: "/ok" })
+    );
+
+    app.get("/ok", function (request, response) {
+        response.send({ nick: request.user.email })
     });
-})
 
-app.post('/loginUsuario', passport.authenticate("local", { failureRedirect: "/fallo", successRedirect: "/ok" })
-);
+    app.get("/agregarUsuario/:nick", function (request, response) {
+        let nick = request.params.nick;
+        let res = sistema.agregarUsuario(nick);
+        response.send(res);
+    });
 
-app.get("/ok", function (request, response) {
-    response.send({ nick: request.user.email })
-});
+    app.get("/obtenerUsuarios",haIniciado, function (request, response) {
+        let res = sistema.obtenerUsuarios();
+        response.send(res);
+    });
 
-app.get("/agregarUsuario/:nick", function (request, response) {
-    let nick = request.params.nick;
-    let res = sistema.agregarUsuario(nick);
-    response.send(res);
-});
+    app.get("/eliminarUsuario/:nick", function (request, response) {
+        let nick = request.params.nick;
+        res = sistema.eliminarUsuario(nick);
+        response.send(res);
+    });
 
-app.get("/obtenerUsuarios", function (request, response) {
-    let res = sistema.obtenerUsuarios();
-    response.send(res);
-});
+    app.get("/numeroUsuarios", haIniciado, function (request, response) {
+        let num = sistema.numeroUsuarios();
+        response.send({ "numero": num });
+    });
 
-app.get("/eliminarUsuario/:nick", function (request, response) {
-    let nick = request.params.nick;
-    res = sistema.eliminarUsuario(nick);
-    response.send(res);
-});
+    app.get("/usuarioActivo/:nick", function (request, response) {
+        let nick = request.params.nick;
+        let res = sistema.usuarioActivo(nick);
+        response.send({ "activo": res });
+    });
 
-app.get("/numeroUsuarios", function (request, response) {
-    let num = sistema.numeroUsuarios();
-    response.send({ "numero": num });
-});
-
-app.get("/usuarioActivo/:nick", function (request, response) {
-    let nick = request.params.nick;
-    let res = sistema.usuarioActivo(nick);
-    response.send({ "activo": res });
-});
-
-app.listen(PORT, () => {
-    console.log(`App está escuchando en el puerto ${PORT}`);
-    console.log('Ctrl+C para salir');
-});
+    app.listen(PORT, () => {
+        console.log(`App está escuchando en el puerto ${PORT}`);
+        console.log('Ctrl+C para salir');
+    });
